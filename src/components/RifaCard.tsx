e52b50b582c9676685f73a2a381d2501afb6e030
Calebe <calebexmz9@gmail.com>
2025-07-19 17:18:05 -0300

    Pagina de status

--- src/components/RifaCard.tsx
+++ src/components/RifaCard.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { ShoppingCart, Minus, Plus, Trophy } from "lucide-react";
 import FormularioCheckout from "./FormularioCheckout";
+import QRCodeDisplay from "./QrCodeDisplay";
 
 interface RifaCardProps {
   rifa: {
